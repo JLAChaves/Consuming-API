3a16c855fa64f9169651955e485264f361b51026
JLAChaves <lucas2015nxi@gmail.com>
2022-05-27 15:52:59 -0400

    Api implemented

--- Consuming-Api/Program.cs
+++ Consuming-Api/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Consuming_Api.Repositories;
+using Refit;
+
+namespace Consuming_Api
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var cepClient = RestService.For<ICepApiServices>("http://viacep.com.br");
+                Console.Write("Informe o CEP: ");
+                string cepInformed = Console.ReadLine().ToString();
+                Console.WriteLine($"Consultando informações do CEP: {cepInformed}");
+
+                var address = await cepClient.GetAddressAsync(cepInformed);
+                Console.WriteLine($"\nLogradouro: {address.Logradouro},\nBairro: {address.Bairro},\nCidade: {address.Localidade}" +
+                    $"\nIBGE: {address.IBGE}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Erro na consulta do Cep: {e.Message}");
+            }
+        }
+    }
+}
---
+++ Consuming-Api/Repositories/ICepApiServices.cs
@@ -0,0 +1,17 @@
+﻿using Consuming_API.Models;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consuming_Api.Repositories
+{
+    public interface ICepApiServices
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepResponse> GetAddressAsync(string cep);
+
+    }
+}
